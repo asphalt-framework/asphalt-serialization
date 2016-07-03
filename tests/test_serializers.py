9402abf7223985b61af61e5b564db929402d3b6a
Alex Grönholm <alex.gronholm@nextday.fi>
2016-07-03 18:09:22 +0300

    Refactored serializer tests

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -2,12 +2,13 @@ import re
 from types import SimpleNamespace
 
 import pytest
+from msgpack import ExtType
+
 from asphalt.serialization.serializers.cbor import CBORSerializer
 from asphalt.serialization.serializers.json import JSONSerializer
 from asphalt.serialization.serializers.msgpack import MsgpackSerializer
 from asphalt.serialization.serializers.pickle import PickleSerializer
 from asphalt.serialization.serializers.yaml import YAMLSerializer
-from msgpack import ExtType
 
 
 class SimpleType:
@@ -57,15 +58,20 @@ class UnserializableSimpleType:
         self.value_b = value_b
 
 
-@pytest.fixture(params=[
-    CBORSerializer,
-    JSONSerializer,
-    MsgpackSerializer,
-    PickleSerializer,
-    YAMLSerializer
-], ids=['cbor', 'json', 'msgpack', 'pickle', 'yaml'])
-def serializer(request):
-    return request.param()
+@pytest.fixture(params=['cbor', 'json', 'msgpack', 'pickle', 'yaml'])
+def serializer_type(request):
+    return request.param
+
+
+@pytest.fixture
+def serializer(serializer_type):
+    return {
+        'cbor': CBORSerializer,
+        'json': JSONSerializer,
+        'msgpack': MsgpackSerializer,
+        'pickle': PickleSerializer,
+        'yaml': YAMLSerializer
+    }[serializer_type]()
 
 
 @pytest.mark.parametrize('input', [
@@ -83,8 +89,7 @@ def test_basic_types_roundtrip(serializer, input):
     assert deserialized == input
 
 
-@pytest.mark.parametrize('serializer', [CBORSerializer(), PickleSerializer(), YAMLSerializer()],
-                         ids=['cbor', 'pickle', 'yaml'])
+@pytest.mark.parametrize('serializer_type', ['cbor', 'pickle', 'yaml'])
 def test_circular_reference(serializer):
     a = {'foo': 1}
     b = {'a': a}
@@ -99,70 +104,59 @@ def test_circular_reference(serializer):
     assert other_b['a'] is other_a
 
 
-@pytest.mark.parametrize('serializer', [CBORSerializer(), MsgpackSerializer(), JSONSerializer()],
-                         ids=['cbor', 'msgpack', 'json'])
-@pytest.mark.parametrize('cls', [SimpleType, SlottedSimpleType], ids=['normal', 'slotted'])
-def test_marshal_unmarshal(serializer, cls):
-    serializer.register_custom_type(cls)
-    testval = cls(1, {'a': 1})
-    testval2 = cls(2, testval)
-    output = serializer.serialize(testval2)
+@pytest.mark.parametrize('serializer_type', ['cbor', 'msgpack', 'json'])
+class TestCustomTypes:
+    @pytest.mark.parametrize('cls', [SimpleType, SlottedSimpleType], ids=['normal', 'slotted'])
+    def test_marshal_unmarshal(self, serializer, cls):
+        serializer.register_custom_type(cls)
+        testval = cls(1, {'a': 1})
+        testval2 = cls(2, testval)
+        output = serializer.serialize(testval2)
+        outval = serializer.deserialize(output)
+        assert outval == testval2
 
-    outval = serializer.deserialize(output)
-    assert outval == testval2
+    def test_custom_state(self, serializer):
+        """Test that marshallers and umarshallers can be embedded into the relevant class."""
+        serializer.register_custom_type(CustomStateSimpleType, CustomStateSimpleType.marshal,
+                                        CustomStateSimpleType.unmarshal)
+        testval = CustomStateSimpleType('a', 1)
+        output = serializer.serialize(testval)
+        outval = serializer.deserialize(output)
+        assert outval == testval
 
+    def test_missing_getattr(self, serializer):
+        testval = UnserializableSimpleType(1, 'a')
+        serializer.register_custom_type(UnserializableSimpleType)
+        exc = pytest.raises(TypeError, serializer.serialize, testval)
+        assert str(exc.value) == ("'test_serializers.UnserializableSimpleType' has no __dict__ "
+                                  "attribute and does not implement __getstate__()")
 
-@pytest.mark.parametrize('serializer', [CBORSerializer(), MsgpackSerializer(), JSONSerializer()],
-                         ids=['cbor', 'msgpack', 'json'])
-def test_custom_state(serializer):
-    """Test that marshallers and umarshallers can be embedded into the relevant class."""
-    serializer.register_custom_type(CustomStateSimpleType, CustomStateSimpleType.marshal,
-                                    CustomStateSimpleType.unmarshal)
-    testval = CustomStateSimpleType('a', 1)
-    output = serializer.serialize(testval)
-    outval = serializer.deserialize(output)
-    assert outval == testval
+    def test_missing_setattr(self, serializer):
+        testval = UnserializableSimpleType(1, 'a')
+        serializer.register_custom_type(UnserializableSimpleType, lambda instance: {})
+        serialized = serializer.serialize(testval)
+        exc = pytest.raises(Exception, serializer.deserialize, serialized)
+        assert str(exc.value).endswith(
+            "'test_serializers.UnserializableSimpleType' has no __dict__ attribute and does not "
+            "implement __setstate__()")
 
+    def test_missing_marshaller(self, serializer_type, serializer):
+        serializer.register_custom_type(SlottedSimpleType)
+        testval = SimpleType(1, 'a')
+        exc = pytest.raises(Exception, serializer.serialize, testval)
+        if serializer_type == 'cbor':
+            assert str(exc.value).endswith('cannot serialize type SimpleType')
+        else:
+            assert str(exc.value) == 'no marshaller found for type "test_serializers.SimpleType"'
 
-@pytest.mark.parametrize('serializer', [CBORSerializer(), MsgpackSerializer(), JSONSerializer()],
-                         ids=['cbor', 'msgpack', 'json'])
-def test_missing_getattr(serializer):
-    testval = UnserializableSimpleType(1, 'a')
-    serializer.register_custom_type(UnserializableSimpleType)
-    exc = pytest.raises(TypeError, serializer.serialize, testval)
-    assert str(exc.value) == ("'test_serializers.UnserializableSimpleType' has no __dict__ "
-                              "attribute and does not implement __getstate__()")
-
-
-@pytest.mark.parametrize('serializer', [CBORSerializer(), MsgpackSerializer(), JSONSerializer()],
-                         ids=['cbor', 'msgpack', 'json'])
-def test_missing_setattr(serializer):
-    testval = UnserializableSimpleType(1, 'a')
-    serializer.register_custom_type(UnserializableSimpleType, lambda instance: {})
-    serialized = serializer.serialize(testval)
-    exc = pytest.raises(Exception, serializer.deserialize, serialized)
-    assert str(exc.value).endswith("'test_serializers.UnserializableSimpleType' has no __dict__ "
-                                   "attribute and does not implement __setstate__()")
-
-
-@pytest.mark.parametrize('serializer', [MsgpackSerializer(), JSONSerializer()],
-                         ids=['msgpack', 'json'])
-def test_missing_marshaller(serializer):
-    serializer.register_custom_type(SlottedSimpleType)
-    testval = SimpleType(1, 'a')
-    exc = pytest.raises(LookupError, serializer.serialize, testval)
-    assert str(exc.value) == 'no marshaller found for type "test_serializers.SimpleType"'
-
-
-@pytest.mark.parametrize('serializer', [CBORSerializer(), MsgpackSerializer(), JSONSerializer()],
-                         ids=['cbor', 'msgpack', 'json'])
-def test_missing_unmarshaller(serializer):
-    serializer.register_custom_type(SlottedSimpleType)
-    serializer.register_custom_type(SimpleType, unmarshaller=None)
-    testval = SimpleType(1, 'a')
-    serialized = serializer.serialize(testval)
-    exc = pytest.raises(Exception, serializer.deserialize, serialized)
-    assert str(exc.value).endswith('no unmarshaller found for type "test_serializers.SimpleType"')
+    def test_missing_unmarshaller(self, serializer):
+        serializer.register_custom_type(SlottedSimpleType)
+        serializer.register_custom_type(SimpleType, unmarshaller=None)
+        testval = SimpleType(1, 'a')
+        serialized = serializer.serialize(testval)
+        exc = pytest.raises(Exception, serializer.deserialize, serialized)
+        assert str(exc.value).endswith(
+            'no unmarshaller found for type "test_serializers.SimpleType"')
 
 
 def test_mime_types(serializer):
@@ -175,7 +169,7 @@ def test_yaml_safe_attribute(safe):
     assert serializer.safe is safe
 
 
-@pytest.mark.parametrize('serializer', [MsgpackSerializer()], ids=['msgpack'])
+@pytest.mark.parametrize('serializer_type', ['msgpack'])
 def test_msgpack_exttype_passthrough(serializer):
     serializer.register_custom_type(SlottedSimpleType)
     ext = ExtType(6, b'somedata')
@@ -186,7 +180,7 @@ def test_msgpack_exttype_passthrough(serializer):
     assert obj.data == b'somedata'
 
 
-@pytest.mark.parametrize('serializer', [CBORSerializer()], ids=['cbor'])
+@pytest.mark.parametrize('serializer_type', ['cbor'])
 def test_cbor_self_referential_objects(serializer):
     value1 = SimpleNamespace()
     value1.val = 1
