87ae0a331f131ac31725c32e7591adf6af5cd4fc
Alex Grönholm <alex.gronholm@nextday.fi>
2017-04-16 05:40:59 +0300

    Made the component tests a bit more robust

--- tests/test_component.py
+++ tests/test_component.py
@@ -14,21 +14,21 @@ async def test_component_start():
         'pickle': {'protocol': 3},
         'yaml': {'safe': False}
     })
-    ctx = Context()
-    await component.start(ctx)
+    async with Context() as ctx:
+        await component.start(ctx)
 
-    assert ctx.json
-    assert ctx.msgpack
-    assert ctx.pickle
-    assert ctx.yaml
+        assert ctx.json
+        assert ctx.msgpack
+        assert ctx.pickle
+        assert ctx.yaml
 
 
 @pytest.mark.asyncio
 async def test_default_config():
     component = SerializationComponent(backend='json')
-    ctx = Context()
-    await component.start(ctx)
+    async with Context() as ctx:
+        await component.start(ctx)
 
-    resource = await ctx.request_resource(Serializer)
-    assert isinstance(resource, JSONSerializer)
-    assert ctx.json is resource
+        resource = ctx.require_resource(Serializer)
+        assert isinstance(resource, JSONSerializer)
+        assert ctx.json is resource
