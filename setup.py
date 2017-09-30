6274ee861b4fc4337f3317b887e496ced053e509
Alex Grönholm <alex.gronholm@nextday.fi>
2017-10-01 02:25:25 +0300

    Updated packaging/testing/documentation configuration

--- setup.py
+++ setup.py
@@ -1,61 +1,12 @@
-from pathlib import Path
-
 from setuptools import setup
 
 setup(
-    name='asphalt-serialization',
     use_scm_version={
         'version_scheme': 'post-release',
         'local_scheme': 'dirty-tag'
     },
-    description='Serialization component for the Asphalt framework',
-    long_description=Path(__file__).with_name('README.rst').read_text('utf-8'),
-    author='Alex Grönholm',
-    author_email='alex.gronholm@nextday.fi',
-    url='https://github.com/asphalt-framework/asphalt-serialization',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ],
-    license='Apache License 2.0',
-    zip_safe=False,
-    packages=[
-        'asphalt.serialization',
-        'asphalt.serialization.serializers'
-    ],
     setup_requires=[
+        'setuptools >= 36.2.7',
         'setuptools_scm >= 1.7.0'
-    ],
-    install_requires=[
-        'asphalt >= 3.0, < 5.0',
-        'typeguard ~= 2.0'
-    ],
-    extras_require={
-        'msgpack': 'msgpack-python >= 0.4.6',
-        'cbor': 'cbor2 ~= 4.0',
-        'yaml': 'ruamel.yaml >= 0.12',
-        'testing': [
-            'pytest',
-            'pytest-cov',
-            'pytest-catchlog',
-            'pytest-asyncio >= 0.5.0'
-        ]
-    },
-    entry_points={
-        'asphalt.components': [
-            'serialization = asphalt.serialization.component:SerializationComponent'
-        ],
-        'asphalt.serialization.serializers': [
-            'cbor = asphalt.serialization.serializers.cbor:CBORSerializer [cbor]',
-            'json = asphalt.serialization.serializers.json:JSONSerializer',
-            'msgpack = asphalt.serialization.serializers.msgpack:MsgpackSerializer [msgpack]',
-            'pickle = asphalt.serialization.serializers.pickle:PickleSerializer',
-            'yaml = asphalt.serialization.serializers.yaml:YAMLSerializer [yaml]'
-        ]
-    }
+    ]
 )
